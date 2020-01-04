de59a81bc765d1eba251cdf90ca73768fb458584
farleybsd <farley.t.i@hotmail.com>
2020-01-03 21:24:32 -0300

    Manipulando Eventos

--- doc/src/App.js
+++ doc/src/App.js
@@ -61,14 +61,28 @@ import './App.css';
 
 class App extends React.Component {
 
+constructor(props)
+{
+  super(props)
+  {
+    this.state = {date : new Date()}
+  }
+}
   
   render(){
-    const props = {name:'teste'}
+   // const props = {name:'teste'}
+
+   const element = <welcome name ="Sara"/>
     return(
-    <h1>Hello,{props.name}</h1>
+    //<h1>Hello,{props.name}</h1>
+    //element
+    <div>
+      <h1>Hello, world !</h1>
+    <h2>It is {this.state.date.toDateString()}</h2>
+    </div>
     )
   }
 }
 export default App;
 
-// Parei aqui https://pt-br.reactjs.org/docs/components-and-props.html
+// Parei aqui https://pt-br.reactjs.org/docs/handling-events.html
