0f7c010d2ed292f5e1bb8754863d509ff03caa42
farleybsd <farley.t.i@hotmail.com>
2020-01-04 21:34:08 -0300

    Acessibilidade

--- doc/src/App.js
+++ doc/src/App.js
@@ -61,28 +61,99 @@ import './App.css';
 
 class App extends React.Component {
 
-constructor(props)
-{
-  super(props)
-  {
-    this.state = {date : new Date()}
-  }
-}
-  
-  render(){
-   // const props = {name:'teste'}
+// constructor(props)
+// {
+//   super(props)
+//   {
+//     this.state = {date : new Date()}
+//   }
+// }
 
-   const element = <welcome name ="Sara"/>
+// constructor(props){
+//   super(props)
+//   this.state = {isToogleOn:true}
+//   this.handleClick = this.handleClick.bind(this);
+//   this.NumberList = this.NumberList.bind(this)
+// }
+constructor(props) {
+  super(props);
+  this.handleChange = this.handleChange.bind(this);
+  this.state = {temperatura: ''};
+}
+
+
+
+handleClick() {
+  this.setState(state => ({
+    isToogleOn: !state.isToogleOn
+    
+  }));
+}
+
+   NumberList(props) {
+     console.log('oi')
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+boilingVerdict(props){
+  if(props.celsius >= 100){
+    return <p>Á agua esta fervida</p>
+  }
+  return <p>Á agua não está fervida</p>
+}
+
+handleChange(e){
+  this.setState({temperatura: e.target.value})
+}
+  render(){
+
+    const temperatura = this.state.temperatura
+
+   // const props = {name:'teste'}
+    // function teste(){
+    //   console.log('clicado')
+    // }
+
+  //  const element = <welcome name ="Sara"/>
+  //  const numbers = [1,2,3,4,5]
+  //  const listItens = numbers.map((number) => number * 2)
+
     return(
+     <fieldset>
+        <legend>Informe a temperatura em Celsius:</legend>
+        <input
+          value={temperatura}
+          onChange={this.handleChange} />
+
+        <boilingVerdict
+          celsius={parseFloat(temperatura)} />
+
+      </fieldset>
+    )
+
     //<h1>Hello,{props.name}</h1>
     //element
-    <div>
-      <h1>Hello, world !</h1>
-    <h2>It is {this.state.date.toDateString()}</h2>
-    </div>
-    )
+    // <div>
+    //   <h1>Hello, world !</h1>
+    // <h2>It is {this.state.date.toDateString()}</h2>
+    // <button onClick={teste}>Clique Aqui</button>
+    // </div>
+    //<button onClick={this.handleClick}> {this.state.isToogleOn ? 'ON ' : 'Off'}</button>
+    
+  //<li>{listItens}</li>
   }
+    
+
+    
+  
 }
 export default App;
 
-// Parei aqui https://pt-br.reactjs.org/docs/handling-events.html
+// Parei aqui https://pt-br.reactjs.org/docs/accessibility.html
